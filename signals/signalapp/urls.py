9f7a26d12ead9defb937d9a5149fee5b527ca72d
Viral <viral.citrusbug@gmail.com>
2022-02-08 12:13:26 +0530

    Added Class Based View in Project

--- signals/signalapp/urls.py
+++ signals/signalapp/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from . import views
+# from .views import login    
 urlpatterns = [
     path('', views.home , name = "home"),
     path('login', views.user_login , name = "login")
+    # path('login', login.as_view() , name = "login")
 ]
 
--- signals/signalapp/views.py
+++ signals/signalapp/views.py
@@ -1,33 +1,52 @@
-import email
-import imp
-from signal import signal
+
+import profile
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.dispatch import Signal, receiver
 from django.db.models.signals import post_save
-
 from signalapp.models import Profile
+from django.views import View
 # Create your views here.
 
 my_signal = Signal(providing_args= ['name'])
 
 def home(request):
-    return HttpResponse("You are at Home")
+    return render(request , "home.html")
 
 
-def user_login(request):
-    if request.method == "POST":
+## FUNCTION BASED VIEW
+
+# def user_login(request):
+#     if request.method == "POST":
+#         fisrtName = request.POST.get('fname')
+#         lastName = request.POST.get('lname')
+#         userName = request.POST.get('uname')
+#         Email = request.POST.get("email")
+#         user = User.objects.create(first_name = fisrtName , last_name = lastName , username = userName , email = Email)
+#         user.save()
+#         print(request.POST)
+#     return render(request , 'login.html')
+
+
+## CLASS BASED VIEW
+
+class login(View):
+    def get(self , request):
+        return render(request , "login.html")
+    def post(self ,request):
         fisrtName = request.POST.get('fname')
         lastName = request.POST.get('lname')
         userName = request.POST.get('uname')
         Email = request.POST.get("email")
         user = User.objects.create(first_name = fisrtName , last_name = lastName , username = userName , email = Email)
         user.save()
-        print(request.POST)
-
-
-    return render(request , 'login.html')
+        a = "ViralPatel"
+        c ={
+            "a" : a
+        }
+        return render(request , "login.html",c)
+        
 
 @receiver(post_save , sender = User )
 def Prof(sender , instance , created , **kwargs):

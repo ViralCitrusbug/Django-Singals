3ebec5dc7d6f9cc6f834b27cf6edcb0b1982e63a
Viral <viral.citrusbug@gmail.com>
2022-02-08 17:23:47 +0530

    Djangp Advanced Completed

--- signals/signalapp/views.py
+++ signals/signalapp/views.py
@@ -1,14 +1,15 @@
-
-import profile
 from django.shortcuts import render
-from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.dispatch import Signal, receiver
 from django.db.models.signals import post_save
 from signalapp.models import Profile
 from django.contrib import messages
 from django.views import View
+
 # Create your views here.
+
+
+## SIGNAL
 
 my_signal = Signal(providing_args= ['name'])
 
@@ -43,17 +44,16 @@ class login(View):
         user = User.objects.create(first_name = fisrtName , last_name = lastName , username = userName , email = Email)
         user.save()
         messages.success(request , "Your Account Has been Successfully Created")
-        a = "ViralPatel"
-        context ={
-            "a" : a
-        }
-        return render(request , "login.html",context)
+        return render(request , "login.html")
         
 
+
+#3 SIGNAL FUNCTION
 @receiver(post_save , sender = User )
 def Prof(sender , instance , created , **kwargs):
     if created:
         prof = Profile.objects.create(user = instance)
+        prof.save()
         print(instance,created,kwargs,sender)
 
 
